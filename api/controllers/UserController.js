b4172578f1cc8da50960cdf9f1a64eb45dab0264
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2014-04-08 12:33:33 -0400

    fixed authentication issues

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -19,7 +19,6 @@ module.exports = {
 
 
   new: function(req, res){
-
     res.view();
   },
 
@@ -28,11 +27,14 @@ module.exports = {
     User.create(req.params.all(), function userCreated(err, user){
 
       if (err) {
-        console.log(err);
+
         req.session.flash = { err: err };
 
         return res.redirect('/user/new');
       }
+
+      req.session.authenticated = true;
+      req.session.User = user;
 
       res.redirect('/user/show/'+user.id);
     });
